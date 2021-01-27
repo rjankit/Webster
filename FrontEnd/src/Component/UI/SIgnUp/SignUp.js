6d3f1de954a91315e3fc170742d82f1812ff8f2c
rjankit <ankit000aryan@gmail.com>
2021-01-28 01:39:56 +0530

    Updated UI

---
+++ FrontEnd/src/Component/UI/SIgnUp/SignUp.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./SignUp.css";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+const SignUp = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    photoUrl: "",
+  });
+  const history = useHistory();
+  const handleLogin = () => {
+    history.push("/");
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log(user);
+    axios.post("http://localhost:5000/createAccount", user).then((response) => {
+      console.log(response.data);
+    });
+  };
+  return (
+    <div className="signup">
+      <img
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+        alt=""
+      />
+
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+
+        <input
+          type="text"
+          placeholder="Profile Pic Url(Optional)"
+          value={user.photoUrl}
+          onChange={(e) => setUser({ ...user, photoUrl: e.target.value })}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button type="submit" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </form>
+
+      <p>
+        Already a member?{" "}
+        <span className="login_register" onClick={handleLogin}>
+          Login
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ FrontEnd/src/Component/UI/Widgets/Widgets.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Widgets.css";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+const Widgets = () => {
+  const newsArticle = (heading, subtitle) => {
+    return (
+      <div className="widgets_article">
+        <div className="widgets_articleLeft">
+          <FiberManualRecordIcon />
+        </div>
+        <div className="widgets_articleRight">
+          <h4>{heading}</h4>
+          <p>{subtitle}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="widgets">
+      <div className="widgets_header">
+        <h2>
+          Webster News
+          <InfoIcon />
+        </h2>
+      </div>
+
+      {newsArticle("Ankit Raj is back", "Top news")}
+      {newsArticle("Ankit Raj is back", "Top news")}
+      {newsArticle("Ankit Raj is back", "Top news")}
+      {newsArticle("Ankit Raj is back", "Top news")}
+    </div>
+  );
+};
+
+export default Widgets;
